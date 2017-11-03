57c53bc09beefe29867f9af4cfb1267ec148bc6b
Dan Gale <danjgale@gmail.com>
2017-11-03 17:10:00 -0400

    initial commit

---
+++ src/scrape.r
@@ -0,0 +1,10 @@
+# ------------------------------------------------------------------------------
+# Scrape Pitchf/x data into local sqlite database
+# 
+# Daniel Gale 2017 (c)
+# ------------------------------------------------------------------------------
+
+library(dplyr)
+library(pitchRx)
+library(baseballr)
+library(DBI)
