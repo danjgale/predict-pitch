670ea32935556cbbf35ef89c93f4737f0115e090
Dan Gale <danjgale@gmail.com>
2017-11-11 23:17:11 -0500

    fixed up functions and removed script portion

--- src/scrape_data.R
+++ src/scrape_data.R
@@ -10,12 +10,30 @@ library(baseballr)
 library(DBI)
 library(RSQLite)
 
-populatePitchDB <- function(timerange){
-  con <- dbConnect(SQLite(), dbname = "../data/pitches.sqlite3")
+getPitches <- function(timerange, db){
+  con <- dbConnect(SQLite(), dbname = db)
   scrape(start = timerange[1], end = timerange[2], connect = con)
   dbDisconnect(con)
 }
 
-start_date <- "2016-04-03"
-end_date <- "2017-11-03"
-populatePitchDB(c(opening_2016,end_date))
+addPlayers <- function(db){
+  con <- dbConnect(SQLite(), dbname = db)
+  dbWriteTable(con, value = players, name = "players", append = TRUE)
+  dbDisconnect(con)
+}
+
+addGameDays <- function(db){
+  con <- dbConnect(SQLite(), dbname = db)
+  dbWriteTable(con, value = gids, name = "gamedays", append = TRUE)
+  dbDisconnect(con)
+}
+
+populateDB <- function(timerange, db){
+  getPitches(timerange, db)
+  addPlayers(db)
+  # addGameDays(db)
+}
+
+
+
+
