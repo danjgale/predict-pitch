a71bd51b312e1e0b0c252f5dbcf300806af03b42
Dan Gale <danjgale@gmail.com>
2017-11-11 23:31:15 -0500

    initial commit of simple database script

---
+++ src/setup_db.R
@@ -0,0 +1,24 @@
+# ------------------------------------------------------------------------------
+# Setup database 
+# 
+# Daniel Gale 2017 (c)
+# ------------------------------------------------------------------------------
+
+library(dplyr)
+library(pitchRx)
+library(baseballr)
+library(DBI)
+library(RSQLite)
+
+source("db_functions.R")
+
+database <- "../data/pitches.sqlite3"
+start_date <- "2016-04-03"
+end_date <- "2017-11-03"
+scrape = FALSE
+  
+if (scrape){
+  # toggle scraping data from pitchfx 
+  getPitches(c(start_date, end_date), database)
+}
+addPlayers(db)
