454cde78046f362a67e373e96f9c54a871726dbe
Dan Gale <danjgale@gmail.com>
2017-11-27 13:54:04 -0500

    included 2015 in data scraping

--- src/setup_db.R
+++ src/setup_db.R
@@ -14,12 +14,12 @@ source("db_functions.R")
 
 # script params
 database <- "../data/pitches.sqlite3"
-start_date <- "2016-04-03"
+start_date <- "2015-04-05"
 end_date <- "2017-11-03"
-scrape = FALSE
+scrape = TRUE
   
 if (scrape){
   # toggle scraping data from pitchfx 
   getPitches(c(start_date, end_date), database)
 }
-addPlayers(db)
+addPlayers(database)
