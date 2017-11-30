d1d7c4eff5be072f434b923cb1137a42e14f42f6
Dan Gale <danjgale@gmail.com>
2017-11-30 14:21:18 -0500

    initial commit

---
+++ src/setup/pitcher_stats.R
@@ -0,0 +1,19 @@
+# ------------------------------------------------------------------------------
+# Script to build pitch trajectories (i.e. "snapshots")
+# 
+# Daniel Gale 2017 (c)
+# ------------------------------------------------------------------------------
+
+rm(list=ls())
+
+# library(Lahman)
+library(baseballr)
+library(DBI)
+library(RSQLite)
+
+pitcher_stats <-  daily_pitcher_bref('2017-04-02', '2017-10-02')
+
+con <- dbConnect(SQLite(), '../data/pitches.sqlite3')
+dbWriteTable(con, 'snapshots', pitcher_stats, overwrite=TRUE)
+dbDisconnect(con)
+
