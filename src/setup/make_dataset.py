16c4d98e9b7ee9631fcd7a35620a5facb6920f3b
Dan Gale <danjgale@gmail.com>
2017-12-17 22:41:50 -0500

    added compute_release_time and adjusted locations from trajectories to match up with actual release

--- src/setup/make_dataset.py
+++ src/setup/make_dataset.py
@@ -92,7 +92,19 @@ def parse_trajectories(x):
     return np.array(json.loads(x))
 
 
-def get_location(x, time=180, from_plate=True):
+def compute_release_time(vy0, actual=55):
+    """Compute how far the ball has travelled between the actual release point
+    (default is 55ft as per consensus) and the initial point recorded by
+    pitchfx. X is
+    """
+
+    dist = 50 - actual
+    ms_per_foot = 1000 / vy0
+
+    return ms_per_foot * dist
+
+
+def get_location(x, time=180, from_plate=False):
     """Get pitch location at specified time; generate an x, y, z location
     array
     """
@@ -122,11 +134,21 @@ if __name__ == '__main__':
     db = PitchDB()
     df = generate_table(db)
 
+    print('Parsing counts...')
     counts = parse_count(df['count'])
 
+    print('Computing adjusted strike zones...')
     zone_height = compute_zone_height(df)
     df = df.merge(zone_height, on='batter')
 
+    df['time_from_release'] = compute_release_time(df['vy0'])
+    mean_time_from_release = np.around(df['time_from_release'].mean(), -1)
+    print(
+        'Rounded average distance from consensus '
+        'release point of 55ft: {}ms'.format(mean_time_from_release)
+    )
+
+    print("Computing strikes...")
     is_strike = df.apply(
         lambda x: call_pitch(x['px'], x['pz'], [
                              x['adj_sz_top'], x['adj_sz_bot']]),
@@ -134,20 +156,18 @@ if __name__ == '__main__':
     )
     is_strike.name = 'is_strike'
 
-    print('Computing location at 180ms')
-    loc_180 = df['trajectories'].apply(lambda x: get_location(x, time=180))
+    print('Computing location at 140ms from y0; total t approx. 175ms')
+    loc_175 = df['trajectories'].apply(lambda x: get_location(x, time=140))
 
-    print('Computing location at 220ms')
-    loc_220 = df['trajectories'].apply(lambda x: get_location(x, time=220))
+    print('Computing location at 190ms from y0; total t approx. 225ms')
+    loc_225 = df['trajectories'].apply(lambda x: get_location(x, time=190))
 
     df.drop('trajectories', axis=1, inplace=True)
 
     df = pd.concat(
-        [df, expand_location(loc_180, ['tx_180', 'ty_180', 'tz_180']),
-         expand_location(loc_220, ['tx_220', 'ty_220', 'tz_220']),
+        [df, expand_location(loc_175, ['tx_175', 'ty_175', 'tz_175']),
+         expand_location(loc_225, ['tx_225', 'ty_225', 'tz_225']),
          is_strike, counts], axis=1)
-
-
 
     db.create(df, 'dataset')
     db.close()
